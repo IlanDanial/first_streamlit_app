64808ab1592a41114bb5e02516d7c8af73fd6c19
IlanDanial <118387439+IlanDanial@users.noreply.github.com>
2023-04-29 13:40:25 -0400

    Update streamlit_app.py
    mispelling of avocado

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,9 +9,9 @@ streamlit.header("Breakfast Favorites")
 streamlit.text("🥣 Omega 3 and Blueberry Oatmeal")
 streamlit.text("🥗 Kale, Spinach and Rocket Smoothie")
 streamlit.text("🐔 Hard-Boiled Free-Range Egg")
-streamlit.text("🥑🍞 Avacado Toast")
+streamlit.text("🥑🍞 Avocado Toast")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ["Avacado", "Strawberries"])
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),["Avocado", "Strawberries"])
 streamlit.dataframe(my_fruit_list)
